d9ff172ea92e798cee231e986ec60f5a74500737
Aaron M <amitchtech@gmail.com>
2019-06-18 19:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ console.log(string.toLowerCase())
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
+  } if else (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else (string === "I love you, Grandma.") {
+    return "I love you too."
   }
 }
 
