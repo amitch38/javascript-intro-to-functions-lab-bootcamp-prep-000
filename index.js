d86ff78d539219a9a6631c79e7d99864fa4391b6
Aaron M <amitchtech@gmail.com>
2019-06-18 18:45:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log(string).toUpperCase()
+}
+
 var uppercase = "HELLO!"
 
 uppercase.toUpperCase() === uppercase
