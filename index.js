b129db4647fff7748ebaaba360b7f988791ab287
Aaron M <amitchtech@gmail.com>
2019-06-18 18:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  return string.toUpperCase()
+function logShout(console.log()) {
+  return console.log().toUpperCase()
 }
 
 function logWhisper(string) {
